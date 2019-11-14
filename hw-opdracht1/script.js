b5d79964a7282c1f1b60a99a8f7171a4a3a8708b
donnevspankeren <donne.van.spankeren01@gmail.com>
2019-11-14 15:24:11 +0100

    commit 2

--- hw-opdracht1/script.js
+++ hw-opdracht1/script.js
@@ -1,3 +1,10 @@
-var getal1 = document.getElementById("veld1").value;
-var getal2 = document.getElementById("veld2").value;
-
+function rekenUit(){
+    var getal1 = document.getElementById("veld1").value;
+    var getal2 = document.getElementById("veld2").value;
+    
+    
+    if (getal1 > getal2){
+        alert(getal1 + getal2);
+        
+    }
+}
